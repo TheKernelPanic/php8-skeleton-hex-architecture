c1636360e3ace8eed26e25c17504f88167eee0ec
KernelPanic <kernelpanic0x000007C4@protonmail.com>
2022-09-11 20:20:58 +0200

    Render method.

--- src/Infrastructure/Http/Controller/BaseController.php
+++ src/Infrastructure/Http/Controller/BaseController.php
@@ -3,7 +3,12 @@ declare(strict_types=1);
 
 namespace Php8SkeletonHexArchitecture\Infrastructure\Http\Controller;
 
+use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\ContainerInterface;
+use Psr\Container\NotFoundExceptionInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\Exception\HttpInternalServerErrorException;
 
 abstract class BaseController
 {
@@ -14,5 +19,32 @@ abstract class BaseController
     public function __construct(
         protected ContainerInterface $container
     ){
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface $response
+     * @param string $template
+     * @param array $data
+     * @return ResponseInterface
+     */
+    protected function render(
+        ServerRequestInterface $request,
+        ResponseInterface $response,
+        string $template, array
+        $data = []): ResponseInterface
+    {
+        try {
+            return $this->container->get('view')->render(
+                response: $response,
+                template: $template,
+                data: $data
+            );
+        } catch (ContainerExceptionInterface|NotFoundExceptionInterface $exception) {
+            throw new HttpInternalServerErrorException(
+                request: $request,
+                message: $exception->getMessage()
+            );
+        }
     }
 }
