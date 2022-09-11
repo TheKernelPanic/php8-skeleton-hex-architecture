34dc26f0bcb702865fe95617034b9e892b306674
KernelPanic <kernelpanic0x000007C4@protonmail.com>
2022-09-11 17:33:42 +0200

    Init slim application.

--- bootstrap.php
+++ bootstrap.php
@@ -1,10 +1,20 @@
 <?php
 declare(strict_types=1);
 
+use DI\ContainerBuilder;
 use Symfony\Component\Dotenv\Dotenv;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
 (new Dotenv())->load(
-    path: __DIR__ . '/env'
+    path: __DIR__ . '/.env'
 );
+
+$containerBuilder = new ContainerBuilder();
+
+$parameters = require_once __DIR__ . '/config/DI/Parameters.php';
+$parameters($containerBuilder);
+
+$containerBuilder->useAnnotations(false);
+
+return $containerBuilder->build();
---
+++ public/index.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+use Php8SkeletonHexArchitecture\Application\Handler\HttpErrorHandler;
+use Slim\Factory\AppFactory;
+use Slim\Factory\ServerRequestCreatorFactory;
+
+$container = require_once __DIR__ . '/../bootstrap.php';
+
+AppFactory::setContainer(
+    container: $container
+);
+
+$application = AppFactory::create();
+
+$routes = require_once __DIR__ . '/../config/Slim/Routes/Common.php';
+$routes($application);
+
+$serverRequest = ServerRequestCreatorFactory::create()->createServerRequestFromGlobals();
+$isDebugMode = $container->get('parameters')['environment_mode'] === 'DEV';
+
+$httpErrorHandler = new HttpErrorHandler(
+    callableResolver: $application->getCallableResolver(),
+    responseFactory: $application->getResponseFactory()
+);
+$shutdownHandler = new \Php8SkeletonHexArchitecture\Application\Handler\ShutdownHandler(
+    request: $serverRequest,
+    httpErrorHandler: $httpErrorHandler,
+    debugMode: $isDebugMode
+);
+$errorMiddleware = $application->addErrorMiddleware(
+    displayErrorDetails: $isDebugMode,
+    logErrors: $isDebugMode,
+    logErrorDetails: $isDebugMode
+);
+$errorMiddleware->setDefaultErrorHandler(
+    handler: $httpErrorHandler
+);
+
+$application->run();
